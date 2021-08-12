a3f5d9c6d4aff973851ea715e384a73b116c4097
Eliza Arzanukaeva <eliza.arzanukaeva@gmail.com>
2021-08-12 14:47:59 +0200

    <img src="/img/react.png" alt="react" title="react" />

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
-import ./App.css
+import './App.css'
 
 function App() {
   return (
 <>
-
+<select>
+  <option value="1">one</option>
+  <option value="2">two</option>
+</select>
+<hr />
+<img src="/img/react.png" alt="react" title="react" />
 </>
     );
 }
